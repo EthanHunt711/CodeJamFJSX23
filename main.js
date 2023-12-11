e9e36934ab2dff48e9f211e78bd46256f182fd6f
EthanHunt711 <79079092+EthanHunt711@users.noreply.github.com>
2023-12-11 19:16:01 +0100

    running and working

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//global variables for general usage
+
 let startRange = document.getElementById('rangeStart');
 let endRange = document.getElementById('rangeEnd');
 let userGuess = document.getElementById('guess');
@@ -19,34 +21,45 @@ function randomNumber(max, min){
 
 
 function guessGame(){
+    //listen for getting the start of the range (inclusive)
     startRange.addEventListener('input', event =>{
-        rangeList[0] = startRange.value;
-        
+        rangeList[0] = Number(startRange.value);
     });
-
+    //listen for getting the end of the range (inclusive)
     endRange.addEventListener('input', event =>{
-        rangeList[1] = endRange.value;
+        rangeList[1] = Number(endRange.value);
        
     });
+    //set the range and lock further changes to the range
     document.querySelector('#setBtn').addEventListener('click', event => {
         startRange.disabled = true;
         endRange.disabled = true;
-
+        
+        // get the secret number
         hiddenValue = randomNumber(rangeList[0], rangeList[1]);
         document.getElementById('setBtn').disabled = true;
         document.getElementById('numberDescription').innerHTML = `Please select a number between ${rangeList[0]} and ${rangeList[1]}`
         
     });
 
-    userGuess.addEventListener('input', event => {
-        userGuessValue = userGuess.value;
-        attempts.push(userGuessValue);
-    });
+    
+    
+
 
     document.querySelector('#btn').addEventListener('click', event => {
+        //get the users input and add it to an array
+        userGuessValue = userGuess.value;
+        attempts.push(Number(userGuessValue));
+        
+
+
+        //add to the counter with each click (guess)
         counter += 1;
 
-        if (userGuessValue > rangeList[0] && userGuessValue < rangeList[1]){
+       
+
+        // check if the input is less than range
+        if (userGuessValue >= rangeList[0] && userGuessValue <= rangeList[1]){
             if (userGuessValue == hiddenValue){
                 document.getElementById('rightInput').innerHTML = `Bull's Eye!!! ${userGuessValue} is the correct guess`;
             } else if(userGuessValue > hiddenValue){
@@ -54,25 +67,28 @@ function guessGame(){
             } else {
                 document.getElementById('errorInput').innerHTML = `Sorry!!! ${userGuessValue} is lower than the secret number`;
             };
-            console.log(counter);
-            console.log(attempts[counter]);
+           
             const lastAttempt = document.createElement('li');
-            lastAttempt.textContent = `Attempt ${counter} was ${attempts[counter]}`;
+            lastAttempt.textContent = `Attempt ${counter} was ${attempts[counter - 1]}`;
             document.querySelector('.previousGuessesList').appendChild(lastAttempt);
+   
         } else {
+            
             document.getElementById('errorInput').innerHTML = `Sorry!!! ${userGuessValue} is out of bounds`;
             const lastAttempt = document.createElement('li');
-            lastAttempt.textContent = `Attempt ${counter} was out of bounds`;
+            lastAttempt.textContent = `Attempt ${counter} (${userGuessValue}) was out of bounds`;
             document.querySelector('.previousGuessesList').appendChild(lastAttempt);
+            
+
         };
-        
+
+    
     });
 
-        
-    
-    
+    document.querySelector('#refreshBtn').addEventListener('click', event =>{
+        window.location.reload();
+    });
 };
-
 
 
 guessGame()
